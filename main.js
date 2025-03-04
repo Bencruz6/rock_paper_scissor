10967d7f7b386635edda289be658c2f7fc7654ce
Benher <199085821+Bencruz6@users.noreply.github.com>
2025-03-04 15:11:28 +0530

    computerlogic func works now after I put the code in the function

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // console.log("Hello World");
-function getComputerChoice()
-{
-
+function getComputerChoice() {
+    if ((Math.floor(Math.random() * 3)) === 0) return "Rock";
+    else if ((Math.floor(Math.random() * 3)) === 0) return "Paper";
+    else return "scissors";
 }
-if((Math.floor(Math.random()*3))===0) return "Rock";
-else if((Math.floor(Math.random()*3))===0) return "Paper";
-else return "scissors";
- 
+
+
 
 // console.log(Math.random()*3);
