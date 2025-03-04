39407a84851ddd44bd8a34092446a6039519efc2
Benher <199085821+Bencruz6@users.noreply.github.com>
2025-03-04 15:10:03 +0530

    random computer logic generated created

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ function getComputerChoice()
 {
 
 }
+if((Math.floor(Math.random()*3))===0) return "Rock";
+else if((Math.floor(Math.random()*3))===0) return "Paper";
+else return "scissors";
+ 
+
+// console.log(Math.random()*3);
