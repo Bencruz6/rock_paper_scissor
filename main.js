0a02ae2579c70a63a323d3390382d7b850222dfa
Benher <199085821+Bencruz6@users.noreply.github.com>
2025-03-11 21:23:20 +0530

    code completed but could be better for sure

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 
 let HumanScore = 0, ComputerScore = 0;
+
+
+
 function getComputerChoice() {
     if ((Math.floor(Math.random() * 3)) === 0) return "Rock";
     else if ((Math.floor(Math.random() * 3)) === 0) return "Paper";
@@ -14,26 +17,63 @@ function getHumanChoice() {
 }
 
 
+
+
 function playround(getComputerChoice, getHumanChoice) {
 
     // we are capitalizing the human input to make it non-case sensitive
     getHumanChoice = getHumanChoice.toUpperCase()
     getComputerChoice = getComputerChoice.toUpperCase();
     const win = `You win ${getHumanChoice} beats ${getComputerChoice}`;
-    const loose = `You lose ${getComputerChoice} beats ${getHumanChoice}`
+    const loose = `You lose ${getComputerChoice} beats ${getHumanChoice}`;
 
-    if (getComputerChoice == 'ROCK' && getHumanChoice == 'PAPER') return win;
-    else if(getComputerChoice == 'ROCK' && getHumanChoice == 'SCISSORS') return loose;
-    else if(getComputerChoice=='PAPER' && getHumanChoice=='SCISSORS') return loose;
-    else if(getComputerChoice=='PAPER' && getHumanChoice=='ROCK') return win;
-    else if(getComputerChoice=='SCISSORS' && getHumanChoice=='ROCK') return win;
-    else if(getComputerChoice=='SCISSORS' && getHumanChoice=='PAPER') return win;
+
+    if (getComputerChoice == 'ROCK' && getHumanChoice == 'PAPER') {
+        HumanScore++;
+        return win;
+    }
+    else if (getComputerChoice == 'ROCK' && getHumanChoice == 'SCISSORS') {
+        ComputerScore++;
+        return loose;
+    }
+    else if (getComputerChoice == 'PAPER' && getHumanChoice == 'SCISSORS') {
+        HumanScore++;
+        return win;
+    }
+    else if (getComputerChoice == 'PAPER' && getHumanChoice == 'ROCK') {
+        ComputerScore++;
+        return loose;
+    }
+    else if (getComputerChoice == 'SCISSORS' && getHumanChoice == 'ROCK') {
+        HumanScore++;
+        return win;
+    }
+    else if (getComputerChoice == 'SCISSORS' && getHumanChoice == 'PAPER') {
+        HumanScore++;
+        return win;
+    }
     else return "draw";
-    
+
 }
 
 
+// console.log(playround(getComputerChoice(),getHumanChoice()));
+
+function playgame(){
+    let i=0;
+    while (i<5) {
+        let x = playround(getComputerChoice(),getHumanChoice());
+        console.log(x);
+        console.log(ComputerScore);
+        console.log(HumanScore);
+        i++;
+    }
+
+     if(ComputerScore<HumanScore) console.log(`You win as your score was ${HumanScore} and computers was ${ComputerScore}`);
+     else if(HumanScore<ComputerScore) console.log(`You loose as your score was ${HumanScore} and computers was ${ComputerScore}`);
+     else console.log('somethign is off')
+
+}
 
 
-console.log(playround(getComputerChoice(), getHumanChoice()));
-
+ playgame();
